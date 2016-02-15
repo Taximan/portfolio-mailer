f72ae3cf180a3e1e69cdd574a3d6a80b3cfeb5d6
Dawid <dcz31249@gmail.com>
2016-02-15 13:03:15 +0100

    reformat tabs to 2 spaces

--- index.js
+++ index.js
@@ -9,23 +9,23 @@ var bodyParser = require('body-parser');
 var pad = require('pad');
 
 var config = {
-	mail: {
-		name: process.env.MAILNAME,
-		pass: process.env.MAILPASS,
-		host: process.env.HOST,
-		serv: process.env.SERVER,
-		sendTo: process.env.SENDTO
-	},
+  mail: {
+    name: process.env.MAILNAME,
+    pass: process.env.MAILPASS,
+    host: process.env.HOST,
+    serv: process.env.SERVER,
+    sendTo: process.env.SENDTO
+  },
 };
 
 // show env variables on bootstrap
 Object.keys(config.mail).forEach(k => {
-	if (k === 'pass' && config.mail.pass) { // mask pasword
-		console.log(pad(k, 8) + chalk.red('=> ') + config.mail[k].split('').map(_ => '*').join(''));
-	} else {
-		console.log(pad(k, 8) + chalk.red('=> ') + config.mail[k]);		
-	}
-});	
+  if (k === 'pass' && config.mail.pass) { // mask pasword
+    console.log(pad(k, 8) + chalk.red('=> ') + config.mail[k].split('').map(_ => '*').join(''));
+  } else {
+    console.log(pad(k, 8) + chalk.red('=> ') + config.mail[k]);   
+  }
+}); 
 
 var transporter = nodemailer.createTransport('smtps://' + config.mail.name + '%40' + config.mail.host + ':' + config.mail.pass + '@' + config.mail.serv);
 
@@ -33,69 +33,69 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/sendmail', function (req, res) {
-	res.sendStatus(200);
+  res.sendStatus(200);
 });
 
 app.post('/sendmail', function (req, res) {
 
-	var body = req.body;
+  var body = req.body;
 
-	var hasAllRequiredFields = true;
-	var requiredFields = ['firstName', 'lastName', 'email', 'message'];
-	var mailTimeStamp = +new Date;
+  var hasAllRequiredFields = true;
+  var requiredFields = ['firstName', 'lastName', 'email', 'message'];
+  var mailTimeStamp = +new Date;
 
-	requiredFields.forEach(function (field) {
-		if(!_.has(body, field)) hasAllRequiredFields = false;
-	});
+  requiredFields.forEach(function (field) {
+    if(!_.has(body, field)) hasAllRequiredFields = false;
+  });
 
-	if(!hasAllRequiredFields) { 
-		res.status(402).send({message: 'missing one of [ ' + requiredFields.reduce(function (acc, field) {
-			return acc.concat(' ' + field);
-		}, '') + ' ]'}); // example: {message: "missing one of [ firstName lastName email message ]"}
-		return;
-	}
+  if(!hasAllRequiredFields) { 
+    res.status(402).send({message: 'missing one of [ ' + requiredFields.reduce(function (acc, field) {
+      return acc.concat(' ' + field);
+    }, '') + ' ]'}); // example: {message: "missing one of [ firstName lastName email message ]"}
+    return;
+  }
 
-	var author = body.firstName + ' ' + body.lastName + ' <' + body.email + '>';
+  var author = body.firstName + ' ' + body.lastName + ' <' + body.email + '>';
 
-	var mailOptions = {
-		from: author,
-		to: config.mail.sendTo,
-		subject: 'mail from portfolio #' + mailTimeStamp,
-		text: body.message + '\n\n\n\ -------\n ' + author
-	};
+  var mailOptions = {
+    from: author,
+    to: config.mail.sendTo,
+    subject: 'mail from portfolio #' + mailTimeStamp,
+    text: body.message + '\n\n\n\ -------\n ' + author
+  };
 
-	console.log('sending mail... #' + mailTimeStamp);
+  console.log('sending mail... #' + mailTimeStamp);
 
-	transporter.sendMail(mailOptions, function (error, info) {
-		
-		if(error) {
-			console.log(chalk.red('mail ' + mailTimeStamp + 'has failed, see log for more info.'));
-		}
+  transporter.sendMail(mailOptions, function (error, info) {
+    
+    if(error) {
+      console.log(chalk.red('mail ' + mailTimeStamp + 'has failed, see log for more info.'));
+    }
 
-		fs.writeFile(
-			'./logs/' + mailTimeStamp + '.json',
-			JSON.stringify(merge(mailOptions, {status: error ? 'failure' : 'success'}, error || info)),
-			function (err) {
-				if (err) {
-					console.log(chalk.red('nvm the log has exploded as well here is what I know'));
-					console.log(err);
-					throw err;
-				}
-				
-				if(!error) {
-					console.log(chalk.green('mail ' + mailTimeStamp + 'sent with success!'));
-					res.status(201).send({message: 'mail sent successfully'});
-				} 
+    fs.writeFile(
+      './logs/' + mailTimeStamp + '.json',
+      JSON.stringify(merge(mailOptions, {status: error ? 'failure' : 'success'}, error || info)),
+      function (err) {
+        if (err) {
+          console.log(chalk.red('nvm the log has exploded as well here is what I know'));
+          console.log(err);
+          throw err;
+        }
+        
+        if(!error) {
+          console.log(chalk.green('mail ' + mailTimeStamp + 'sent with success!'));
+          res.status(201).send({message: 'mail sent successfully'});
+        } 
 
-				else {
-					console.log(error);
-					res.status(500).send({message: 'something went wrong >.<'});
-				}
+        else {
+          console.log(error);
+          res.status(500).send({message: 'something went wrong >.<'});
+        }
 
-			}		
-		);
+      }   
+    );
 
-	});
+  });
 
  
 });
